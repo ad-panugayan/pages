d1fd74c2f39a88b891f6038afc26a01ee105a0e6
angel <adrianpanugayan@gmail.com>
2020-12-03 07:34:33 +0000

    RBAC

--- src/main/java/org/dell/kube/pages/PageController.java
+++ src/main/java/org/dell/kube/pages/PageController.java
@@ -1,7 +1,9 @@
 package org.dell.kube.pages;
 
+import feign.FeignException;
 import org.slf4j.LoggerFactory;
 import ch.qos.logback.classic.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -12,16 +14,44 @@ public class PageController {
 
     Logger logger =(Logger) LoggerFactory.getLogger(this.getClass());
 
+    @Autowired
+    private CategoryClient categoryClient;
     private IPageRepository pageRepository;
     public PageController(IPageRepository pageRepository)
     {
         this.pageRepository = pageRepository;
     }
+
     @PostMapping
     public ResponseEntity<Page> create(@RequestBody Page page) {
-        Page newPage= pageRepository.create(page);
-        return new ResponseEntity<Page>(newPage, HttpStatus.CREATED);
+
+        logger.info("CREATE-INFO:Creating a new page");
+        logger.debug("CREATE-DEBUG:Creating a new  page");
+        Category category = null;
+        try {
+            category = categoryClient.findCategory(page.getCategoryId());
+        }
+        catch(FeignException ex){
+            if(ex.getMessage().contains("404")) {
+                return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+            }
+            else{
+                return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+            }
+        }
+
+        if(category ==null || category.getId()==null) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+        else
+        {
+            Page newPage = pageRepository.create(page);
+            logger.info("CREATE-INFO:Created a new page with id = " + newPage.id);
+            logger.debug("CREATE-DEBUG:Created a new  page with id = " + newPage.id);
+            return new ResponseEntity<Page>(newPage, HttpStatus.CREATED);
+        }
     }
+
     @GetMapping("{id}")
     public ResponseEntity<Page> read(@PathVariable long id) {
 
